7160f1bd2258e2c42d3bde7c0bcdea3fd69ef087
Faisal Budiono <ucul.project@gmail.com>
2024-11-29 01:29:04 +0700

    ref: change entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,22 @@
 #!/bin/sh -l
 
-/app -base $1 -access $2 -app $3
+BASE=$1
+ACCESS_TOKEN=$2
+APP_ID=$3
+
+FLAG_BASE=""
+if [ ! -z $BASE ]; then
+    FLAG_BASE="-base $BASE"
+fi
+
+FLAG_ACCESS_TOKEN=""
+if [ ! -z $ACCESS_TOKEN ]; then
+    FLAG_ACCESS_TOKEN="-access $ACCESS_TOKEN"
+fi
+
+FLAG_APP_ID=""
+if [ ! -z $APP_ID ]; then
+    FLAG_APP_ID="-app $APP_ID"
+fi
+
+/app $FLAG_BASE $FLAG_ACCESS_TOKEN $FLAG_APP_ID
